06c121d4b965047027bacf58175bb8a99952faa5
cgardner <chris.r.gardner@gmail.com>
2015-11-27 16:15:24 -0500

    Updated

--- DemoApp/app/js/controllers/EditEventController.js
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -2,8 +2,12 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        $scope.saveEvent = function (event) {
-            window.alert('event' + event.name);
+        $scope.saveEvent = function (event, newEventForm) {
+            console.log(newEventForm);
+            
+            if (newEventForm.$valid) {
+                window.alert('event ' + event.name + ' saved!');
+            }
             
         };
         
